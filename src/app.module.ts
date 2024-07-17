4d16b103b1d1a0dfee58c6bb8c1cb21e777d1c9a
choewy <choewy32@gmail.com>
2024-07-17 23:16:50 +0900

    feature: redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { ConfigurationModule } from '@core';
-import { DatabaseModule } from '@infra';
+import { DatabaseModule, RedisModule } from '@infra';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigurationModule.forRoot(), DatabaseModule.forRoot()],
+  imports: [ConfigurationModule.forRoot(), DatabaseModule.forRoot(), RedisModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/infra/redis/decorators/index.ts
@@ -0,0 +1,1 @@
+export * from './inject-redis-client';
