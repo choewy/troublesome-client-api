c6f31445dd7066689b4219009cd515dd93a61160
choewy <choewy32@gmail.com>
2024-07-20 19:49:38 +0900

    refactor: compare object values

--- src/common/helpers/compare-object.ts
+++ src/common/helpers/compare-object.ts
@@ -1,10 +1,10 @@
 import { instanceToPlain } from 'class-transformer';
 
 export const compareObjectValues = (target: object, origin: object) => {
-  let isSame = true;
-
   const targetObject = instanceToPlain(target, { enableCircularCheck: true, enableImplicitConversion: true });
   const originObject = instanceToPlain(origin, { enableCircularCheck: true, enableImplicitConversion: true });
+
+  let isSame = true;
 
   for (const key of Object.keys(targetObject)) {
     const targetValue = targetObject[key];
@@ -14,20 +14,14 @@ export const compareObjectValues = (target: object, origin: object) => {
       continue;
     }
 
-    if (targetValue === null || originValue === null) {
-      isSame = false;
-      break;
-    }
-
-    if (typeof targetValue === 'object' && typeof originValue === 'object') {
-      isSame = compareObjectValues(targetValue, originValue);
-
-      if (isSame === true) {
+    if (typeof targetValue === 'object' && typeof originValue === 'object' && targetValue !== null && originValue !== null) {
+      if (compareObjectValues(targetValue, originValue)) {
         continue;
       }
     }
 
     isSame = false;
+    break;
   }
 
   return isSame;
