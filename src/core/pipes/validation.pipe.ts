ec2c2e74b7b2f4cbe0a12975f34dd659b8e77f42
choewy <choewy32@gmail.com>
2024-09-28 18:58:24 +0900

    fix: validation pipe exception

--- src/core/pipes/validation.pipe.ts
+++ src/core/pipes/validation.pipe.ts
@@ -1,4 +1,6 @@
-import { Injectable, ValidationPipe as NestValidationPipe } from '@nestjs/common';
+import { HttpStatus, Injectable, ValidationPipe as NestValidationPipe } from '@nestjs/common';
+
+import { Exception } from '../filters';
 
 @Injectable()
 export class ValidationPipe extends NestValidationPipe {
@@ -11,7 +13,17 @@ export class ValidationPipe extends NestValidationPipe {
       },
       stopAtFirstError: true,
       exceptionFactory(errors) {
-        return errors[0];
+        const error = errors[0];
+        const constraints = error.constraints;
+        const key = Object.keys(constraints)[0];
+        const handlerName = Array.from(key)
+          .map((k) => (k === k.toUpperCase() ? `_${k}` : k))
+          .join('');
+
+        const name = [error.property, handlerName].join('_').toUpperCase();
+        const message = constraints[key];
+
+        return new Exception('VALIDATION_FAILED', HttpStatus.BAD_REQUEST, { name, message });
       },
     });
   }
