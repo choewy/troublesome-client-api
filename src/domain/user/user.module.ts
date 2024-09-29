a94be05db7b7e689ae29c45ff87753d0d4c9695c
choewy <choewy32@gmail.com>
2024-09-29 13:21:06 +0900

    feature: user module

---
+++ src/domain/user/user.module.ts
@@ -0,0 +1,14 @@
+import { UserEntity } from '@choewy/troublesome-entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
---
+++ src/domain/user/user.service.ts
@@ -0,0 +1,33 @@
+import { UserEntity } from '@choewy/troublesome-entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
+import { EntityManager, Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  protected getRepository(em?: EntityManager) {
+    return em instanceof EntityManager ? em.getRepository(UserEntity) : this.userRepository;
+  }
+
+  async getByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  async hasByEmail(email: string) {
+    return this.userRepository.existsBy({ email });
+  }
+
+  async insert(args: Partial<Pick<UserEntity, 'email' | 'password' | 'name' | 'partnerId' | 'fulfillmentId'>>, em?: EntityManager) {
+    const user = plainToInstance(UserEntity, args);
+
+    await this.getRepository(em).insert(user);
+
+    return user;
+  }
+}
