e153936a2365d87041346cbe7553af2df9ed88bb
choewy <choewy32@gmail.com>
2024-09-30 22:29:08 +0900

    chore: request, response log에 headers 포함

--- src/global/logger/implements/request-log.ts
+++ src/global/logger/implements/request-log.ts
@@ -1,15 +1,22 @@
 import { Request } from 'express';
 
+import { RequestHeader } from '@/common';
+
 export class RequestLog {
   readonly id: number | string | object;
   readonly ip: string;
   readonly method: string;
   readonly url: string;
+  readonly headers: object;
 
   constructor(req: Request) {
     this.id = req['id'];
     this.ip = String(req.headers['x-forwarded-for'] ?? req.ip);
     this.method = req.method;
     this.url = req.url;
+    this.headers = {
+      [RequestHeader.AccessToken]: req.headers[RequestHeader.AccessToken],
+      [RequestHeader.RefreshToken]: req.headers[RequestHeader.RefreshToken],
+    };
   }
 }
--- src/global/logger/implements/response-log.ts
+++ src/global/logger/implements/response-log.ts
@@ -1,10 +1,13 @@
 import { Response } from 'express';
 
+import { ResponseHeader } from '@/common';
+
 export class ResponseLog {
   readonly status: number;
   readonly message: string;
   readonly exception?: unknown;
   readonly error?: unknown;
+  readonly headers: object;
 
   constructor(res: Response) {
     this.status = res.statusCode;
@@ -17,5 +20,11 @@ export class ResponseLog {
     if (res.locals.error) {
       this.exception = res.locals.error;
     }
+
+    this.headers = {
+      [ResponseHeader.RequestID]: res.getHeader(ResponseHeader.RequestID),
+      [ResponseHeader.AccessToken]: res.getHeader(ResponseHeader.AccessToken),
+      [ResponseHeader.RefreshToken]: res.getHeader(ResponseHeader.RefreshToken),
+    };
   }
 }
