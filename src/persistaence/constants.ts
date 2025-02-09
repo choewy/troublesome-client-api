94c0b51c047f141bf286c6b254f4a19a16351a85
choewy <choewy32@gmail.com>
2025-02-09 16:25:23 +0900

    feature: constants

---
+++ src/persistaence/constants.ts
@@ -0,0 +1,6 @@
+export const BACKGROUND_IMAGE_PATHJ = '/images/background.jpg';
+export const TITLE_IMAGE_PATHJ = '/images/title.jpg';
+export const DEVELOPMENT_CARD_IMAGE_PATH = '/images/development-cards';
+export const DEVELOPMENT_CARD_BEHIND_IMAGE = '/images/development-card-behind';
+export const NOBLE_CARD_IMAGE_PATH = '/images/noble-cards';
+export const TOKEN_IMAGE_PATH = '/images/tokens';
--- src/persistaence/helpers.ts
+++ src/persistaence/helpers.ts
@@ -1,5 +1,7 @@
 import { AxiosError, AxiosResponse } from 'axios';
 
+import { DEVELOPMENT_CARD_BEHIND_IMAGE, DEVELOPMENT_CARD_IMAGE_PATH, NOBLE_CARD_IMAGE_PATH, TOKEN_IMAGE_PATH } from './constants';
+
 export type ValueFromHttpReturnType<T> = {
   data: T | null;
   error: AxiosError | null;
@@ -19,3 +21,8 @@ export const valueFromHttp = async <T = any, D = any>(response: Promise<AxiosRes
 
   return result;
 };
+
+export const getDevelopmentCardImagePath = (id: number) => `${DEVELOPMENT_CARD_IMAGE_PATH}/${id}.jpg`;
+export const getDevelopmentCardBehindImagePath = (level: number) => `${DEVELOPMENT_CARD_BEHIND_IMAGE}/${level}.jpg`;
+export const getNobleCardImagePath = (id: number) => `${NOBLE_CARD_IMAGE_PATH}/${id}.jpg`;
+export const getTokenImagePath = (name: string) => `${TOKEN_IMAGE_PATH}/${name}.jpg`;
